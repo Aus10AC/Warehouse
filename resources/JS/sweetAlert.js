ed10af16dbcb7af8bfe58024f5edddb2e5e07e31
Private <36454345+Privateth@users.noreply.github.com>
2018-11-29 17:42:57 -0600

    first commit

---
+++ resources/JS/sweetAlert.js
@@ -0,0 +1,10 @@
+function popUp(){
+  swal({
+    title: "Message Sent!",
+    text: "I'll get in touch with you shortly",
+    icon: "success",
+    button: "close",
+    iconColor: "blue"
+  });
+
+}
